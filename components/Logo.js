0e0673ff8814a594b1d8d4d94f45936bd40e97bf
lorenzo1975 <lorenzo.beltrame@gmail.com>
2021-08-03 10:41:10 +0700

    added basic header, added seo plugin, theme plugin, beginning linting rules

---
+++ components/Logo.js
@@ -0,0 +1,14 @@
+export default function Logo() {
+    return (
+        <div className="text-lg font-semibold tracking-widest rounded-lg focus:outline-none focus:shadow-outline">
+            <div className="inline-flex items-center">
+                <div className="w-2 h-2 p-2 mr-2 bg-pink-600 rounded-full shadow-synthBox">
+                </div>
+                <h2
+                    className="text-lg font-bold tracking-tighter text-white tracking-relaxed">
+                    BuiltOnChain.com
+                </h2>
+            </div>
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+        <Head>
+		      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Mr+Dafoe&display=swap" rel="stylesheet" />
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Monoton&display=swap" rel="stylesheet" />
+        
+        </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
