f6a71ef550bafab248379a8ff6e7ef51a4e0f82f
lorenzo1975 <lorenzo.beltrame@gmail.com>
2021-08-10 16:27:43 +0700

    added basic pages and prose dark mode for mdx articles

--- pages/chains/bitcoin.js
+++ pages/chains/bitcoin.js
@@ -1,6 +1,12 @@
+import { NextSeo } from 'next-seo';
 export default function bitcoin() {
-    return (
-        <section className="justify-center pt-24 body-font">
+  return (
+    <>
+      <NextSeo
+        title="What is bitcoin, explained simple"
+        description="The best explanation of bitcoin"
+      />
+      <section className="justify-center pt-24 body-font">
         <div className="container px-5 mx-auto lg:px-28">
           <div className="flex flex-wrap px-6 lg:px-0 lg:mb-10">
             <h2 className="mb-4 text-2xl font-medium text-fuchsia-400 lg:text-6xl title-font font-dafoe lg:mb-0">
@@ -9,5 +15,17 @@ export default function bitcoin() {
           </div>
         </div>
       </section>
-    )
+      <article className="prose dark:prose-dark lg:prose-xl">
+        <h1>
+          Bticoin, a new monetary system: a World and Interplanetary self
+          sovreign bank!
+        </h1>
+        <p>
+          Explaining bitcoin does not have to be complex. We can skip all the
+          economy and math basics and grasp the ideas and concepts behind it.
+        </p>
+        <p>Bitcoin was created</p>
+      </article>
+    </>
+  );
 }
---
+++ pages/chains/blockchain101.js
@@ -0,0 +1,13 @@
+export default function blockchain101() {
+  return (
+    <section className="justify-center pt-24 body-font">
+      <div className="container px-5 mx-auto lg:px-28">
+        <div className="flex flex-wrap px-6 lg:px-0 lg:mb-10">
+          <h2 className="mb-4 text-2xl font-medium text-fuchsia-400 lg:text-6xl title-font font-dafoe lg:mb-0">
+            What is Blockchain?
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+}
